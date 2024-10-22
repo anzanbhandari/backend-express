ec69605c730a4ec3a97ef2a5dfe81140fa3c6a0b
Anjan Bhandari <anjanbhandari@susankya.com.np>
2024-10-22 07:49:00 +0545

    add video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -77,14 +77,11 @@ userSchema.methods.generateAccessToken = function(){
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullname: this.fullname
+            _id: this._id
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        process.env.RFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.RFRESH_TOKEN_EXPIRY
         }
     )
 }
